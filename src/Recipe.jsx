2e8f777570ec298e43cc2e163ca57f21a6ba3aa2
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-04-15 12:09:56 -0700

    Added Material UI to the Lab, and installed extra packages

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,31 +1,36 @@
+import { Typography } from '@mui/material'; 
+
 // Presentational component
-function Recipe({recipe}) {
-    return (
-      <div className="card">
-        <h2>{recipe.name}</h2>
-        <h3>Ingredients</h3>
-        <ul>
-          {recipe.ingredients.map((item, index) => (
-            <li key={index}>
-              <input type="checkbox" /> {item}
-            </li>))}
-        </ul>
-        <h3>Instructions</h3>
-        <ol>
-          {recipe.instructions.map((step, index) => (<li key={index}>{step}</li>))}
-        </ol>
-        {recipe.optionalGlaze && (
-          <div className="glaze">
-            <h4>Optional: {recipe.optionalGlaze.name}</h4>
-            <ul>
-              {recipe.optionalGlaze.ingredients.map((item, index) => (<li key={index}>{item}</li>))}
-            </ul>
-            <ol>
-              {recipe.optionalGlaze.instructions.map((step, index) => (<li key={index}>{step}</li>))}
-            </ol>
-          </div>)}
-      </div>
-    )
+function Recipe({ recipe }) {
+  return (
+    <div className="card">
+      <h2>{recipe.name}</h2> 
+      <Typography variant="h6" component="h3" gutterBottom>Ingredients</Typography>
+      <ul>
+        {recipe.ingredients.map((item, index) => (
+          <li key={index}>
+            <input type="checkbox" /> {item}
+          </li>
+        ))}
+      </ul>
+      <Typography variant="h6" component="h3" gutterBottom sx={{fontWeight:'bold'}}>Instructions</Typography>
+      <ol>
+        {recipe.instructions.map((step, index) => (<li key={index}>{step}</li>))}
+      </ol>
+      {/* Optional Glaze Section */}
+      {recipe.optionalGlaze && (
+        <div className="glaze">
+          <h4>Optional: {recipe.optionalGlaze.name}</h4>
+          <ul>
+            {recipe.optionalGlaze.ingredients.map((item, index) => (<li key={index}>{item}</li>))}
+          </ul>
+          <ol>
+            {recipe.optionalGlaze.instructions.map((step, index) => (<li key={index}>{step}</li>))}
+          </ol>
+        </div>)}
+    </div>
+  )
 }
-  
-export default Recipe
+
+export default Recipe;
+
